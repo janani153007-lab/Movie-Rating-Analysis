58144ea5b5cadb8f5b824a62816113e1e64fa9dc
janani153007-lab <janani153007@gmail.com>
2026-06-15 14:21:52 +0530

    Create movie-rating-analysis.py

---
+++ movie-rating-analysis.py
@@ -0,0 +1,12 @@
+print("Movie Rating Analysis")
+
+movies = 100
+average_rating = 7.5
+high_rated_movies = 65
+
+print("Total Movies:", movies)
+print("Average Rating:", average_rating)
+print("Highly Rated Movies:", high_rated_movies)
+
+percentage = (high_rated_movies / movies) * 100
+print("Highly Rated Percentage:", round(percentage, 2), "%")
